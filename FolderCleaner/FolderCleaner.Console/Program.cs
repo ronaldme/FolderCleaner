3cb5bcac1466641ad61be04218562c6e2fcfd303
ronaldme <ronaldme@gmail.com>
2019-03-23 14:25:46 +0100

    Add initial projects / code

---
+++ FolderCleaner/FolderCleaner.Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Configuration;
+using FolderCleaner.Core;
+
+namespace FolderCleaner.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var folder = ConfigurationManager.AppSettings["folder"];
+            var excludeFileExtensions = ConfigurationManager.AppSettings["excludeFileExtensions"].Split(',');
+            var excludeSubFolders = ConfigurationManager.AppSettings["excludeSubFolders"].Split(',');
+            var deleteSubFolders = bool.Parse(ConfigurationManager.AppSettings["deleteSubFolders"]);
+
+            var clean = new Cleanup(folder, deleteSubFolders, excludeFileExtensions, excludeSubFolders);
+            var files = clean.ShowFilesToBeRemoved();
+
+            foreach (var file in files)
+            {
+                System.Console.WriteLine($"File to be removed: {file}");
+            }
+
+            System.Console.WriteLine("Running clean...");
+            clean.Run();
+            System.Console.WriteLine($"Cleaned folder: {folder}.");
+            System.Console.Read();
+        }
+    }
+}
---
+++ FolderCleaner/FolderCleaner.Core/Cleanup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace FolderCleaner.Core
+{
+    public class Cleanup
+    {
+        private readonly string _folder;
+        private readonly bool _deleteSubFolders;
+        private readonly string[] _excludeExtensions;
+        private readonly string[] _excludeSubFolders;
+
+        public Cleanup(string folder, bool deleteSubFolders = true, 
+            string[] excludeExtensions = null, string[] excludeSubFolders = null)
+        {
+            _folder = folder;
+            _deleteSubFolders = deleteSubFolders;
+            _excludeExtensions = excludeExtensions;
+            _excludeSubFolders = excludeSubFolders;
+        }
+
+        public void Run()
+        {
+            // todo: Clean folder
+        }
+
+        public List<string> ShowFilesToBeRemoved()
+        {
+            // todo: Return files which will be removed when executing Run
+            return new List<string>();
+        }
+    }
+}
